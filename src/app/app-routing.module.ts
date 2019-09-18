949b641eafb8b7c1156b9615ed22df12d2ed15c2
SRINIVAS-B-SINDAGI <srinivas.sindagi@hebbalelabs.com>
2019-09-18 19:25:54 +0530

    movie-app inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
+import { AppComponent } from 'src/app/app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: 'home-page',
+      pathMatch: 'full'
+    },
+    {
+      path: 'home-page',
+      loadChildren: () => import('src/app/home-page/index').then(m => m.HomePageModule)
+    },
+    {
+      path: 'movie-details/:id',
+      loadChildren: () => import('src/app/movie-details/index').then(m => m.MovieDetailsPageModule)
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
