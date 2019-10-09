0ec08854fbc661fae2aa7e9f8e6c16df18de8b0a
bhooshankalamani <bhooshan.kalamani@hebbalelabs.com>
2019-10-09 18:12:15 +0530

    testing with more delay

--- cypress/integration/examples/App_Test.js
+++ cypress/integration/examples/App_Test.js
@@ -1,41 +1,34 @@
 describe('Movie App Test', function () {
-
   /** 
    *Home page loads up with the search bar 
   */
-
   it('view movie page', function () {
     cy.visit('http://localhost:4200/')
-    // cy.screenshot();
+    cy.get('input');
   })
-
   /** 
    * Type in Keywords
    * Search results come back
   */
-
   it('Search movie ', function () {
-    cy.wait(3000);
-    cy.get('input').type('Batman');
-
+    cy.wait(9000);
+    if (cy.get('input')) {
+      cy.get('input').type('Batman');
+    }
   })
-
   /** 
    * Search results come back
   */
-
   it('Checking Movie List Api Response', function () {
     cy.get('div > .flex-container').find('app-poster').should('to.have.length', 10);
   })
-
   //click on the movie card
   it('Click on movie card to movie detail page', function () {
-    cy.wait(3000);
+    // cy.wait(3000);
     cy.get('div > .flex-container').find('app-poster').first().click();
   })
-
   it('Click on back button ', function () {
-    cy.wait(3000);
+    cy.wait(9000);
     cy.get('.container').find('mat-icon').click();
   })
 })
